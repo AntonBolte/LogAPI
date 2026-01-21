38bf78dcacc182407cb1d26854beccd1ea5804be
Anton Bolte <bolteanton22@gmail.com>
2026-01-21 22:56:22 +0100

    dwada

--- LogAPI_installer.lua
+++ LogAPI_installer.lua
@@ -2,7 +2,6 @@ local rawFileURL = "https://raw.githubusercontent.com/AntonBolte/LogAPI/refs/hea
 local arg = { ... }
 print("Downloading LogAPI...")
 local response = http.get(rawFileURL)
-print("Saving to " .. arg[1] .. " ...")
 if response then
     local file = fs.open(arg[1], "w")
     file.write(response.readAll())
