88db2597b7070bf10941dab00f8f99fb9196cdb9
Anton Bolte <bolteanton22@gmail.com>
2026-01-22 16:27:59 +0100

    I hate thisthing

--- LogAPI_installer.lua
+++ LogAPI_installer.lua
@@ -1,18 +1,9 @@
 local rawFileURL = "https://raw.githubusercontent.com/AntonBolte/LogAPI/refs/heads/master/LogAPI.lua"
-local arg = { ... }
 print("Downloading LogAPI...")
 local response = http.get(rawFileURL)
-
-
-print(response)
-
-if response then
-    local file = fs.open(arg[1], "w")
-    file.write(response.readAll())
-    file.close()
-    print("LogAPI downloaded successfully.")
-else
-    print("Failed to download LogAPI. Please check your internet connection.")
-end
+local file = fs.open("APIs/LogAPI.lua", "w")
+file.write(response.readAll())
+file.close()
+print("LogAPI downloaded successfully.")
 
 response.close()
