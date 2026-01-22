b8532b3b559787cf3468853daf6b3ff4be91fc7a
Anton Bolte <bolteanton22@gmail.com>
2026-01-22 16:58:42 +0100

    i bet theres a better way to test for that but idk it

--- LogAPI.lua
+++ LogAPI.lua
@@ -8,6 +8,10 @@ function Time()
 end
 
 function Install(URL, path)
+    if not path then
+        return {status = "error",
+                reason = "no path given"}
+    end
     local content = http.get(URL)
     local file = fs.open(path, "w+")
     file.write(content.readAll())
@@ -21,36 +25,44 @@ end
 function Comment(message, logName)
     term.setTextColor(colors.white)
     local content = Time() .. "[COMMENT]" .. message
+    if logName then
     local file = fs.open(logPath .. logName .. "Logs.txt", "w")
     file.writeLine(content)
     file.close()
+    end
     print(content)
 end
 
 function Info(message, logName)
     term.setTextColor(colors.blue)
     local content = Time() .. "[INFO]" .. message
+    if logName then
     local file = fs.open(logPath .. logName .. "Logs.txt", "w")
     file.writeLine(content)
     file.close()
+    end
     print(content)
 end
 
 function Warn(message, logName)
     term.setTextColor(colors.yellow)
     local content = Time() .. "[WARN]" .. message
+    if logName then
     local file = fs.open(logPath .. logName .. "Logs.txt", "w")
     file.writeLine(content)
     file.close()
+    end
     print(content)
 end
 
 function Error(message, logName)
     term.setTextColor(colors.red)
     local content = Time() .. "[ERROR]" .. message
+    if logName then
     local file = fs.open(logPath .. logName .. "Logs.txt", "w")
     file.writeLine(content)
     file.close()
+    end
     print(content)
 end
 
